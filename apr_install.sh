0d94d774b79fba3e3d0656827d394425b342acfd
Zoldur <zoldur99@gmail.com>
2018-06-16 03:14:49 -0700

    Get IP

--- apr_install.sh
+++ apr_install.sh
@@ -12,7 +12,7 @@ COIN_NAME='Aprcoin'
 COIN_PORT=3133
 RPC_PORT=3132
 
-NODEIP=$(curl -s4 icanhazip.com)
+NODEIP=$(curl -s4 api.ipify.org)
 
 
 RED='\033[0;31m'
@@ -172,7 +172,7 @@ function get_ip() {
   declare -a NODE_IPS
   for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
   do
-    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
   done
 
   if [ ${#NODE_IPS[@]} -gt 1 ]
